ddc2f5f973570192ddeaa0523b5fba93970e76de
lzxzz <1042183935@qq.com>
2022-04-13 20:17:23 +0800

    上传

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const base=require('./webpack.config.base');
+
+module.exports = {
+  ...base,
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      ...base.module.rules,
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
